650c0d93900f369a437f4c2d78096177f953ea73
Joe Bhullar <joebhullar11@gmail.com>
2021-10-05 11:22:40 -0600

    Add project files.

---
+++ CShrpPractice/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CShrpPractice
+{
+    class Book
+    {
+        public Book(string name) //Constructor method same name as class & Cannot have a return type BECAUSE IT IS A constructor
+        {             // This code will execute everytime we use new keyword for class Book to create constructor for Book();
+            grades = new List<double>(); //This code Garunteed to execute when I use "new" keyword against this calss
+            this.name = name; // Sets FIELD Value 'public string name' = Equal to = Paramater passed 'name'
+                              // this.name is reffering to FIELD 'public string name;'!!!!
+                              //this is an implicit variable available inside methods and inside of constructors. We use it to refer to objects being operated on. 
+        }                                 
+        public void AddGrade(double grade)
+        {
+            this.grades.Add(grade); // Constructor Book();  Will Execute before Invoking AddGrade that will try to use that field. 
+        } //this keyword refers to FIELD Paramater!!!
+        public List<double> grades;//this field is NOW PUBLIC outside of this calss. 
+        public string name; // our FIELD Varaible for Name of book
+    }
+}
---
+++ CShrpPractice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CShrpPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("First Grade Book");
+            book.AddGrade(89.1);
+
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };  //declare new list of double
+            grades.Add(56.1); //we are adding to the list of grades
+
+            var result = 0.0;
+            foreach (var number in grades)
+            {
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The avg grade is {result:N1}");
+      }
+    }
+}
